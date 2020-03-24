14a6ddf079e8354cf3e602ff199ef41e4ddba3e1
Ibrahim Aldarra <ibrahim.aldarra@student.hu.nl>
2020-03-24 10:32:35 +0100

    Klas klasse

---
+++ code/src/nl/rubend/pris/model/Klas.java
@@ -0,0 +1,17 @@
+package nl.rubend.pris.model;
+
+public class Klas {
+	private String klasNaam;
+
+	public Klas(String nm) {
+		this.klasNaam = nm;
+	}
+
+	public String getKlasNaam() {
+		return klasNaam;
+	}
+
+	public void setKlasNaam(String klasNaam) {
+		this.klasNaam = klasNaam;
+	}
+}
