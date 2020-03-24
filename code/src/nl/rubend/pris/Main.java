68db379e88bee82aaef8d6017b7e52fa2bc7511e
Ruben <15885455+RubenNL@users.noreply.github.com>
2020-03-24 10:16:55 +0100

    launches inloggen.fxml

--- code/src/nl/rubend/pris/Main.java
+++ code/src/nl/rubend/pris/Main.java
@@ -2,6 +2,8 @@ package nl.rubend.pris;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -13,21 +15,13 @@ public class Main extends Application {
 	}
 
 	@Override
-	public void start(Stage primaryStage) {
-		primaryStage.setTitle("Hello World!");
-		Button btn = new Button();
-		btn.setText("Say 'Hello World'");
-		btn.setOnAction(new EventHandler<ActionEvent>() {
+	public void start(Stage stage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("userinterface/inloggen.fxml"));
 
-			@Override
-			public void handle(ActionEvent event) {
-				System.out.println("Hello World!");
-			}
-		});
+		Scene scene = new Scene(root);
 
-		StackPane root = new StackPane();
-		root.getChildren().add(btn);
-		primaryStage.setScene(new Scene(root, 300, 250));
-		primaryStage.show();
+		stage.setTitle("FXML Welcome");
+		stage.setScene(scene);
+		stage.show();
 	}
 }
--- code/src/nl/rubend/pris/userinterface/Inloggen.java
+++ code/src/nl/rubend/pris/userinterface/Inloggen.java
@@ -1,4 +1,27 @@
 package nl.rubend.pris.userinterface;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class Inloggen {
+
+	@FXML
+	private TextField emailBox;
+
+	@FXML
+	private PasswordField wachtwoordBox;
+
+	@FXML
+	void cancelButton(ActionEvent event) {
+
+	}
+
+	@FXML
+	void okButton(ActionEvent event) {
+		System.out.println(emailBox.getText());
+		System.out.println(wachtwoordBox.getText());
+	}
+
 }
