0783d19209aea6185a0081ce4f9f91c3d9408a3b
Ruben <15885455+RubenNL@users.noreply.github.com>
2020-03-27 15:19:27 +0100

    docent opgesplitst

--- code/src/nl/rubend/pris/userinterface/Docent/DocentController.java
+++ code/src/nl/rubend/pris/userinterface/Docent/DocentController.java
@@ -4,6 +4,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -25,85 +26,37 @@ import javax.swing.*;
 
 
 public class DocentController implements Initializable, IngelogdGebruiker {
-	@FXML
-	private Button presentieMenuButton;
-
-	@FXML
-	private Button lesMenuButton;
-
-	@FXML
-	private Button langdurigMenuButton;
-
-	@FXML
-	private GridPane gridContainer;
-
-	@FXML
-	private AnchorPane welkomPane;
-
-	@FXML
-	private Label welkomLabel;
-
-	@FXML
-	private AnchorPane presentiePane;
-
-	@FXML
-	private AnchorPane lesPane;
-
-	@FXML
-	private AnchorPane langdurigPane;
-
-	@FXML
-	private ListView studentAanwezigheid;
-
-
+	@FXML private Button presentieMenuButton;
+	@FXML private GridPane gridContainer;
+	@FXML private AnchorPane welkomPane;
+	@FXML private Label welkomLabel;
+	@FXML private Parent docentPresentiePane;
+	@FXML private DocentPresentiePane docentPresentiePaneController;
 	private Docent docent;
-
-	ArrayList<AnchorPane> allPanes = new ArrayList();
-
-	private static Klas klas;
-
-	public static void setKlas(Klas kls) { klas = kls; }
-
+	private ArrayList<Parent> allPanes = new ArrayList<Parent>();
+	private ArrayList<IngelogdGebruiker> paneControllers = new ArrayList<IngelogdGebruiker>();
 	@Override
 	public void setGebruiker(Gebruiker gebruiker) {
 		this.docent=(Docent) gebruiker;
+		for(IngelogdGebruiker controller:paneControllers) {
+			controller.setGebruiker(docent);
+		}
 	}
 
 	@Override
 	public void initialize(URL url, ResourceBundle resourceBundle) {
+		allPanes.add(welkomPane);
+		allPanes.add(docentPresentiePane);
+		paneControllers.add(docentPresentiePaneController);
 		switchToPane(welkomPane);
 		welkomLabel.setText("Welkom ");
-
-		ObservableList<String> opties =
-				FXCollections.observableArrayList(
-						"Aanwezig",
-							 "Afwezig",
-							 "Ziek"
-				);
-		ComboBox aanwezigheidsBox = new ComboBox(opties);
-		ArrayList<Label> namen = new ArrayList<>();
-		for (Student student : klas.getStudenten()) {
-			Label label = new Label(student.getNaam());
-			namen.add(label);
-			label.setFont(new Font("Arial", 12));
-			label.setPadding(new Insets(10,10,5,5));
-		}
-
-		studentAanwezigheid.setItems(FXCollections.observableList(namen));
-
-		for (int i = 1; i < namen.size(); i++) {
-
-		}
-
 	}
 
 	private void switchToPane(AnchorPane targetPane){
 		allPanes.add(welkomPane);
-		allPanes.add(presentiePane);
-		allPanes.add(lesPane);
-		allPanes.add(langdurigPane);
+		allPanes.add(docentPresentiePane);
 
-		for (AnchorPane pane : allPanes){
+		for (Parent pane : allPanes){
 			if (pane.equals(targetPane)) {
 				pane.setVisible(true);
 				pane.setDisable(false);
