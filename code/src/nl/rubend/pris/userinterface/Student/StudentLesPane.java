af2a68540796b83d838809632bd458d8f52f04da
Ibrahim <ibrahim.aldarra@student.hu.nl>
2020-04-01 07:52:01 +0200

    Alle Student laten ook de naam van de student zien

--- code/src/nl/rubend/pris/userinterface/Student/StudentLesPane.java
+++ code/src/nl/rubend/pris/userinterface/Student/StudentLesPane.java
@@ -1,14 +1,17 @@
 package nl.rubend.pris.userinterface.Student;
 
+import javafx.scene.control.Label;
 import nl.rubend.pris.model.Gebruiker;
 import nl.rubend.pris.model.Student;
 import nl.rubend.pris.userinterface.IngelogdGebruiker;
 
 public class StudentLesPane implements IngelogdGebruiker {
+	public Label labelId;
 	private Student student;
 
 	@Override
 	public void setGebruiker(Gebruiker gebruiker) {
 		this.student=(Student) gebruiker;
+		labelId.setText(student.getNaam());
 	}
 }
--- code/src/nl/rubend/pris/userinterface/Student/StudentZiekPane.java
+++ code/src/nl/rubend/pris/userinterface/Student/StudentZiekPane.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.DateCell;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
 import javafx.util.Callback;
 import nl.rubend.pris.model.Gebruiker;
 import nl.rubend.pris.model.Student;
@@ -15,11 +16,13 @@ import java.time.LocalDate;
 import java.util.ResourceBundle;
 
 public class StudentZiekPane implements IngelogdGebruiker,Initializable {
+	public Label labelId;
 	private Student student;
 
 	@Override
 	public void setGebruiker(Gebruiker gebruiker) {
 		this.student=(Student) gebruiker;
+		labelId.setText(student.getNaam());
 	}
 
 	@FXML
