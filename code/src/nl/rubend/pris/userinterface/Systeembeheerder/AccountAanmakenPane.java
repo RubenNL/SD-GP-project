1bd925941826f5c001f25f45a49b1a83b2c6801b
Ruben <15885455+RubenNL@users.noreply.github.com>
2020-04-02 12:10:39 +0200

    accountAanmaken verbeterd

--- code/src/nl/rubend/pris/userinterface/Systeembeheerder/AccountAanmakenPane.java
+++ code/src/nl/rubend/pris/userinterface/Systeembeheerder/AccountAanmakenPane.java
@@ -1,14 +1,9 @@
 package nl.rubend.pris.userinterface.Systeembeheerder;
 
-import javafx.application.Platform;
-import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.paint.Color;
 import nl.rubend.pris.Utils;
 import nl.rubend.pris.model.*;
@@ -24,7 +19,7 @@ public class AccountAanmakenPane implements Initializable, IngelogdGebruiker {
 	@FXML private TextField nieuwAccountName;
 	@FXML private TextField nieuwAccountEmail;
 	@FXML private TextField nieuwAccountWachtwoord;
-	@FXML private TextField nieuwAccountNummer;
+	@FXML private Spinner nieuwAccountNummer;
 	@FXML private ComboBox<String> accountTypeComboBox;
 	@FXML private ComboBox<String> comboBoxGroep;
 	@FXML private Label nummberLabel;
@@ -38,105 +33,75 @@ public class AccountAanmakenPane implements Initializable, IngelogdGebruiker {
 
 	@Override
 	public void initialize(URL url, ResourceBundle resourceBundle) {
-		ArrayList<String> accountTypen = new ArrayList<>();
-		accountTypen.add("Student");
-		accountTypen.add("Docent");
-		accountTypeComboBox.setItems(FXCollections.observableArrayList(accountTypen));
+		nieuwAccountNummer.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 999999999));
+		for(Class gebruikerType:Gebruiker.gebruikerTypes) {
+			accountTypeComboBox.getItems().add(gebruikerType.getSimpleName());
+		}
 	}
 
 	@FXML
 	void maakAccountAan(ActionEvent event) throws Exception {
-
-		String gebruikersType = accountTypeComboBox.getSelectionModel().getSelectedItem();
-		String groep = comboBoxGroep.getSelectionModel().getSelectedItem();
-		if (gebruikersType != null) {
-
-			String naam = nieuwAccountName.getText();
-			String email = nieuwAccountEmail.getText();
-			String wachtwoord = nieuwAccountWachtwoord.getText();
-			try {
-				int nummer = Integer.valueOf(nieuwAccountNummer.getText());
-				if (naam != null && email != null && wachtwoord != null && groep != null) {
-					if (gebruikersType.equals("Student")) {
-						Klas klas = school.getKlasByName(groep);
-						Student newStudent = new Student(email, wachtwoord, naam, nummer);
-						try {
-							errorMeldingLabel.setText("");
-							school.addGebruiker(newStudent);
-							klas.addStudent(newStudent);
-							Utils.melding("Student account geregestreerd!\n"
-									+ "Naam: " + newStudent.getNaam() + "\n"
-									+ "Email: " + newStudent.getEmail() + "\n"
-									+ "Studentnummer: " + newStudent.getStudentNummer() + "\n"
-									+ "En hij zit in : " + newStudent.getKlassen().toString()
-							);
-						} catch (Exception e) {
-							melding(e.getMessage());
-						}
-					}
-					if (gebruikersType.equals("Docent")) {
-						Cursus cursus = school.getCursusByCode(groep);
-						Docent newDocent = new Docent(email, wachtwoord, naam, nummer);
-						try {
-							school.addGebruiker(newDocent);
-							newDocent.setCursus(cursus);
-							errorMeldingLabel.setText("");
-							Utils.melding("Docent account geregestreerd!\n"
-									+ "Naam: " + newDocent.getNaam() + "\n"
-									+ "Email: " + newDocent.getEmail() + "\n"
-									+ "Studentnummer: " + newDocent.getDocentNummer() + "\n"
-									+ "En hij geeft lessen bij de cursus: " + newDocent.getCursus().getCursusNaam()
-							);
-						} catch (Exception e) {
-							melding(e.getMessage());
-						}
-					}
+		if (accountTypeComboBox.getValue().equals("")) {
+			melding("geen accounttype geselecteerd!");
+			return;
+		}
+		Class<Gebruiker> gebruikerType = (Class<Gebruiker>) Class.forName("nl.rubend.pris.model."+accountTypeComboBox.getValue());
+		String naam = nieuwAccountName.getText();
+		String email = nieuwAccountEmail.getText();
+		String wachtwoord = nieuwAccountWachtwoord.getText();
+		String groep = comboBoxGroep.getValue();
+		if (naam != null && email != null && wachtwoord != null) {
+			Gebruiker gebruiker;
+			if (gebruikerType.equals(Systeembeheerder.class)) gebruiker = (Gebruiker) gebruikerType.getDeclaredConstructor(String.class,String.class,String.class).newInstance(email, wachtwoord, naam);
+			else {
+				int nummer = (Integer) nieuwAccountNummer.getValue();
+				if (nummer != 0 && !groep.equals("")) {
+					gebruiker = (Gebruiker) gebruikerType.getDeclaredConstructor(String.class,String.class,String.class,int.class).newInstance(email, wachtwoord, naam, nummer);
+					if (gebruikerType.equals(Student.class)) ((Student) gebruiker).addKlas(school.getKlasByName(groep));
+					else if (gebruikerType.equals(Docent.class)) ((Docent) gebruiker).setCursus(school.getCursusByCode(groep));
 				} else {
-				melding("Ongeldige invoer!");
+					melding("Niet alle gegevens ingevoerd.");
+					return;
 				}
-			} catch (IllegalArgumentException iae) {
-				melding("Voer een geldige student/docent nummer in!");
 			}
-		} else {
-			melding("Kies account type!");
+			school.addGebruiker(gebruiker);
+			Utils.melding("Account is geregistreerd!");
+			nieuwAccountNummer.getValueFactory().setValue(0);
+			nieuwAccountName.setText("");
+			nieuwAccountEmail.setText("");
+			nieuwAccountWachtwoord.setText("");
 		}
 	}
-
-
 
 	@Override
 	public void setGebruiker(Gebruiker gebruiker) {
 		systeembeheerder=(Systeembeheerder) gebruiker;
+		accountTypeComboBox.setValue(Student.class.getSimpleName());
+		accountTypeComboBox.getOnAction().handle(null);
 	}
 	@FXML
 	void handlecomboBoxGebruikersType(ActionEvent actionEvent) {
-		String gebruikersType = accountTypeComboBox.getSelectionModel().getSelectedItem();
-		if (gebruikersType != null) {
-			comboBoxGroep.setDisable(false);
-
-			if (gebruikersType.equals("Student")) {
-				nummberLabel.setText("Studentnummer");
-				hoortBijLabel.setText("Klas");
-				if (klassen!=null) {
-					for (int i = 0; i < klassen.size(); i++) {
-						klassenNamen.add(klassen.get(i).getKlasNaam());
-					}
-					comboBoxGroep.setItems(FXCollections.observableArrayList(klassenNamen));
-				}
-			}
-			if (gebruikersType.equals("Docent")) {
-				nummberLabel.setText("Docentnummer");
-				hoortBijLabel.setText("Cursus Code");
-				if(cursussen!=null) {
-					for (int i = 0; i < cursussen.size(); i++) {
-						cursussCodes.add(cursussen.get(i).getCursusCode());
-					}
-					comboBoxGroep.setItems(FXCollections.observableArrayList(cursussCodes));
-				}
-			}
+		String gebruikersType = accountTypeComboBox.getValue();
+		comboBoxGroep.getItems().removeAll(comboBoxGroep.getItems());
+		nummberLabel.setVisible(true);
+		hoortBijLabel.setVisible(true);
+		nieuwAccountNummer.setVisible(true);
+		comboBoxGroep.setVisible(true);
+		if(gebruikersType.equals("Student")) {
+			nummberLabel.setText("Studentnummer");
+			hoortBijLabel.setText("Klas");
+			for(Klas klas:school.getKlassen()) comboBoxGroep.getItems().add(klas.getKlasNaam());
+		} else if (gebruikersType.equals("Docent")) {
+			nummberLabel.setText("Docentnummer");
+			hoortBijLabel.setText("Cursus Code");
+			for(Cursus cursus:school.getCursussen()) comboBoxGroep.getItems().add(cursus.getCursusCode());
+		} else if (gebruikersType.equals("Systeembeheerder")) {
+			nummberLabel.setVisible(false);
+			hoortBijLabel.setVisible(false);
+			nieuwAccountNummer.setVisible(false);
+			comboBoxGroep.setVisible(false);
 		}
 	}
-
 
 	public void melding(String str) {
 //		Het zou misschien beter met CSS gestyled kunnen
