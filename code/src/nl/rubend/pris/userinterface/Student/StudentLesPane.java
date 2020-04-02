17e3dde211d280f86ce90b1b1c4cf9c581f1523c
Marlon Scholten <57098905+MarlonScholten@users.noreply.github.com>
2020-04-02 14:56:48 +0200

    student les pane aangepast en meer lessen toegevoegd in main

--- code/src/nl/rubend/pris/userinterface/Student/StudentLesPane.java
+++ code/src/nl/rubend/pris/userinterface/Student/StudentLesPane.java
@@ -1,11 +1,13 @@
 package nl.rubend.pris.userinterface.Student;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.DateCell;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.util.Callback;
 import nl.rubend.pris.model.*;
@@ -14,29 +16,64 @@ import nl.rubend.pris.userinterface.IngelogdGebruiker;
 import java.net.URL;
 import java.time.LocalDate;
 import java.util.ResourceBundle;
+import java.util.TreeMap;
 
 public class StudentLesPane implements IngelogdGebruiker, Initializable {
 	private Student student;
 	@FXML DatePicker dateBox;
 	@FXML VBox lessenMenu;
+	@FXML
+	Button lesConfirmButton;
+
+	TreeMap<Integer, String> lesData = new TreeMap<>();
+
 	@FXML private void dateSelect() {
 		lessenMenu.getChildren().removeAll(lessenMenu.getChildren());
 		for(Les les:student.getLessenOpDag(dateBox.getValue())) {
 			Aanwezigheid aanwezigheid=student.getAanwezigheidBijLes(les);
 			boolean checked=aanwezigheid.getStatus()==Aanwezigheid.GEPLAND;
-			CheckBox checkbox=new CheckBox(les.getBeginTijd()+"-"+les.getCursus().getCursusNaam());
+			CheckBox checkbox=new CheckBox(les.getBeginTijd()+" - "+les.getCursus().getCursusNaam());
 			checkbox.setSelected(checked);
-			checkbox.setOnAction(actionEvent ->  {
-				try {
-					if(checkbox.isSelected()) aanwezigheid.setStatus(student,Aanwezigheid.GEPLAND);
-					else aanwezigheid.setStatus(student,Aanwezigheid.AANWEZIG);
-				} catch (NotFoundException e) {
-					e.printStackTrace();
+			checkbox.setPadding(new Insets(0,0,10,0));
+
+			checkbox.selectedProperty().addListener(new ChangeListener<Boolean>() {
+				@Override
+				public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+					if (checkbox.isSelected()){
+						System.out.println("Checked");
+					}
+					else{
+						System.out.println("Unchecked");
+					}
 				}
 			});
 			lessenMenu.getChildren().add(checkbox);
 		}
 	}
+
+	@FXML
+	private void lesConfirm(){
+		System.out.println("yeet");
+//		ObservableList<Node> checkBoxes = lessenMenu.getChildren();
+//		for (Node checkBox : checkBoxes){
+//			if (checkBox instanceof CheckBox){
+//				if (((CheckBox) checkBox).isSelected()){
+//					checkBox
+//				}
+//			}
+//			else{
+//				System.out.println("Not a checkbox");
+//			}
+//		}
+
+//		try {
+//			if(checkbox.isSelected()) aanwezigheid.setStatus(student,Aanwezigheid.GEPLAND);
+//			else aanwezigheid.setStatus(student,Aanwezigheid.AANWEZIG);
+//		} catch (NotFoundException e) {
+//			e.printStackTrace();
+//		}
+	}
+
 	@Override
 	public void setGebruiker(Gebruiker gebruiker) {
 		this.student=(Student) gebruiker;
