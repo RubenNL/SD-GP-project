409eb2caabae2d6b6c27f17e2f2f17523ff00b66
Marlon Scholten <57098905+MarlonScholten@users.noreply.github.com>
2020-04-02 16:34:32 +0200

    Student les pane aangepast, welke les is aangevinkt word soort van getracked.

--- code/src/nl/rubend/pris/userinterface/Student/StudentLesPane.java
+++ code/src/nl/rubend/pris/userinterface/Student/StudentLesPane.java
@@ -29,12 +29,24 @@ public class StudentLesPane implements IngelogdGebruiker, Initializable {
 
 	@FXML private void dateSelect() {
 		lessenMenu.getChildren().removeAll(lessenMenu.getChildren());
+		int i = 0;
 		for(Les les:student.getLessenOpDag(dateBox.getValue())) {
 			Aanwezigheid aanwezigheid=student.getAanwezigheidBijLes(les);
 			boolean checked=aanwezigheid.getStatus()==Aanwezigheid.GEPLAND;
 			CheckBox checkbox=new CheckBox(les.getBeginTijd()+" - "+les.getCursus().getCursusNaam());
 			checkbox.setSelected(checked);
 			checkbox.setPadding(new Insets(0,0,10,0));
+
+			if (checked){
+				lesData.put(i, "Checked");
+				i+= 1;
+			}
+			else{
+				lesData.put(i, "Unchecked");
+				i+=1;
+			}
+
+			System.out.println(lesData);
 
 			checkbox.selectedProperty().addListener(new ChangeListener<Boolean>() {
 				@Override
