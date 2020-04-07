d342d71e3ad1319df65517502e1da7bf7b8fae9d
Ibrahim <ibrahim.aldarra@student.hu.nl>
2020-04-07 10:47:00 +0200

    testfuncties

--- code/src/nl/rubend/prisTest/model/LanguageFilterTest.java
+++ code/src/nl/rubend/prisTest/model/LanguageFilterTest.java
@@ -1,7 +1,40 @@
 package nl.rubend.pris.model;
 
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.Dictionary;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LanguageFilterTest {
+
+    String zin1 = "Hallo bla bla bla bla bla bla schelden bla bla bla";
+    String zin2 = "Hallo bla bla bla bla bla bla bla bla bla bla bla bla";
+    String zin3 = "Hallo bla bla bla bla bla bla scheldenbla bla bla";
+
+    @Test
+    void test_zinMetEenScheldwoord() throws IOException {
+
+        assertFalse(LanguageFilter.isGeschikt(zin1));
+
+    }
+
+    @Test
+    void test_zinZonderScheldwoord() throws IOException {
+
+        assertTrue(LanguageFilter.isGeschikt(zin2));
+
+    }
+
+    @Test
+    void test_zinZonderEenEchtScheldwoord() throws IOException {
+
+        assertTrue(LanguageFilter.isGeschikt(zin3));
+
+    }
+
+
+
 
 }
