e6fa2a4734b866ee98a63b769b3d081167937e59
Ruben <15885455+RubenNL@users.noreply.github.com>
2020-04-07 11:51:11 +0200

    fix voor onnodige foutmeldingen

--- code/src/nl/rubend/prisTest/model/SchoolTest.java
+++ code/src/nl/rubend/prisTest/model/SchoolTest.java
@@ -14,18 +14,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SchoolTest {
 
-	static Student st1, st2;
-
+	private School school;
 	@BeforeEach
 	public void initialize() {
-		try {
-			st1 = new Student("jan@hu.nl", "sssadfsdfsdf", "jan", 1234, null);
-			st2 = new Student("jan@hu.nl", "sssadfsdfsdf", "jan", 1234, null);
-
-		} catch (Exception e) {
-			String errorMessage = "Exception: " + e.getMessage();
-			System.out.println(errorMessage);
-		}
+		School.resetSchool();
+		school=School.getSchool();
 	}
 
 
@@ -33,51 +26,35 @@ class SchoolTest {
 
 	@Test
 	public void test_KlasGelijkAanNullToevoegen() {
-		School school=School.getSchool();
-		try {
-			school.addKlas(null);
-		}
-		catch (IllegalArgumentException iae){
-			assertEquals("Ongeldige waarde", iae.getMessage());
-		}
+		IllegalArgumentException e=assertThrows(IllegalArgumentException.class,()->school.addKlas(null),"expected exception");
+		assertEquals("Ongeldige waarde", e.getMessage());
 	}
 
 
 	@Test
 	public void test_GebruikerGelijkAanNullToevoegen() {
-		School school=School.getSchool();
-		try {
-			school.addGebruiker(null);
-		}
-		catch (IllegalArgumentException iae){
-			assertEquals("Ongeldige waarde", iae.getMessage());
-		}
+		IllegalArgumentException e=assertThrows(IllegalArgumentException.class,()->school.addGebruiker(null),"expected exception");
+		assertEquals("Ongeldige waarde", e.getMessage());
 	}
 
 
 
 	@Test
 	public void test_CursusGelijkAanNullToevoegen() {
-		School school=School.getSchool();
-		try {
-			school.addCursus(null);
-		}
-		catch (IllegalArgumentException iae){
-			assertEquals("Ongeldige waarde", iae.getMessage());
-		}
+		IllegalArgumentException e=assertThrows(IllegalArgumentException.class,()->school.addCursus(null),"expected exception");
+		assertEquals("Ongeldige waarde", e.getMessage());
 	}
 
 
 	@Test
 	void test_generiek_BestaandeGebruikerToevoegen() {
-		School school=School.getSchool();
 		Gebruiker gebruiker1 = new Gebruiker("jan@hu.nl", "sssadfsdfsdf", "jan");
 		school.addGebruiker(gebruiker1);
 		Gebruiker gebruiker2 = new Gebruiker("jan@hu.nl", "sssadfsdfsdf", "jan");
 		IllegalArgumentException thrown = assertThrows(
 				IllegalArgumentException.class,
 				() -> school.addGebruiker(gebruiker2),
-				"Gebruiker (" + gebruiker2 + ") bestaat al"
+				"Zou een foutmelding moeten geven"
 		);
 
 		assertTrue(thrown.getMessage().contains("bestaat al"));
@@ -86,7 +63,6 @@ class SchoolTest {
 
 	@Test
 	void test_dezelfdeSubklasse_BestaandeGebruikerToevoegen() {
-		School school=School.getSchool();
 		Student student1 = new Student("jan@hu.nl", "sssadfsdfsdf", "jan", 1234, null);
 		school.addGebruiker(student1);
 		Student student2 = new Student("jan@hu.nl", "sssadfsdfsdf", "jan", 1234, null);
@@ -103,7 +79,6 @@ class SchoolTest {
 
 	@Test
 	void test_verschillendeSubklasse_BestaandeGebruikerToevoegen() {
-		School school=School.getSchool();
 		Student student = new Student("jan@hu.nl", "sssadfsdfsdf", "jan", 1234, null);
 		school.addGebruiker(student);
 		Docent docent = new Docent("jan@hu.nl", "345efgdgfdgf", "jan", 1234);
@@ -120,7 +95,6 @@ class SchoolTest {
 
 	@Test
 	public void test_BestaandeKlasToevoegen() {
-		School school=School.getSchool();
 		Klas klas1 = new Klas("TICT-SD-V1E");
 		school.addKlas(klas1);
 		Klas klas2 = new Klas("TICT-SD-V1E");
@@ -137,7 +111,6 @@ class SchoolTest {
 
 	@Test
 	public void test_BestaandeCursusToevoegen() {
-		School school=School.getSchool();
 		Cursus cursus1 =  new Cursus("TCIF-V1GP-19_2019","SD-GroupProject");
 		school.addCursus(cursus1);
 		Cursus cursus2 =  new Cursus("TCIF-V1GP-19_2019","SD-GroupProject");
