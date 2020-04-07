f99b665b32e49d3fc98260f66b4efbf245e912e5
Ruben <15885455+RubenNL@users.noreply.github.com>
2020-04-07 11:42:04 +0200

    hoeft geen losse variable te zijn:)

--- code/src/nl/rubend/prisTest/model/LanguageFilterTest.java
+++ code/src/nl/rubend/prisTest/model/LanguageFilterTest.java
@@ -4,38 +4,22 @@ import nl.rubend.pris.model.LanguageFilter;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.util.Dictionary;
-import nl.rubend.pris.model.LanguageFilter;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LanguageFilterTest {
 
-	String zin1 = "Hallo bla bla bla bla bla bla schelden bla bla bla";
-	String zin2 = "Hallo bla bla bla bla bla bla bla bla bla bla bla bla";
-	String zin3 = "Hallo bla bla bla bla bla bla scheldenbla bla bla";
-
 	@Test
 	void test_zinMetEenScheldwoord() throws IOException {
-
-		assertFalse(LanguageFilter.isGeschikt(zin1));
-
+		assertFalse(LanguageFilter.isGeschikt("Hallo bla bla bla bla bla bla schelden bla bla bla"));
 	}
 
 	@Test
 	void test_zinZonderScheldwoord() throws IOException {
-
-		assertTrue(LanguageFilter.isGeschikt(zin2));
-
+		assertTrue(LanguageFilter.isGeschikt("Hallo bla bla bla bla bla bla bla bla bla bla bla bla"));
 	}
 
 	@Test
 	void test_zinZonderEenEchtScheldwoord() throws IOException {
-
-		assertTrue(LanguageFilter.isGeschikt(zin3));
-
+		assertTrue(LanguageFilter.isGeschikt("Hallo bla bla bla bla bla bla scheldenbla bla bla"));
 	}
-
-
-
-
 }
