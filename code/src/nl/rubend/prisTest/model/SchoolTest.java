ba7970269f948a54c18af9a00021143a17f121e8
Ruben <15885455+RubenNL@users.noreply.github.com>
2020-04-08 16:07:52 +0200

    test functies werken weer

--- code/src/nl/rubend/prisTest/model/SchoolTest.java
+++ code/src/nl/rubend/prisTest/model/SchoolTest.java
@@ -11,35 +11,33 @@ class SchoolTest {
 
 	private School school;
 	private Docent slber;
+
 	@BeforeEach
 	public void initialize() {
 		School.resetSchool();
-		school=School.getSchool();
-		slber=new Docent("test123@abdsoifj.nl","asodfijasdf","osjdfasdf",212123);
+		school = School.getSchool();
+		slber = new Docent("test123@abdsoifj.nl", "asodfijasdf", "osjdfasdf", 212123);
 		school.addGebruiker(slber);
 	}
 
 
-
-
 	@Test
 	public void test_KlasGelijkAanNullToevoegen() {
-		IllegalArgumentException e=assertThrows(IllegalArgumentException.class,()->school.addKlas(null),"expected exception");
+		IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> school.addKlas(null), "expected exception");
 		assertEquals("Ongeldige waarde", e.getMessage());
 	}
 
 
 	@Test
 	public void test_GebruikerGelijkAanNullToevoegen() {
-		IllegalArgumentException e=assertThrows(IllegalArgumentException.class,()->school.addGebruiker(null),"expected exception");
-		assertEquals("Ongeldige waarde", e.getMessage());
+		IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> school.addGebruiker(null), "expected exception");
+		assertEquals("Gebruiker is geen Gebruiker?", e.getMessage());
 	}
-
 
 
 	@Test
 	public void test_CursusGelijkAanNullToevoegen() {
-		IllegalArgumentException e=assertThrows(IllegalArgumentException.class,()->school.addCursus(null),"expected exception");
+		IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> school.addCursus(null), "expected exception");
 		assertEquals("Ongeldige waarde", e.getMessage());
 	}
 
@@ -48,10 +46,9 @@ class SchoolTest {
 	void test_generiek_BestaandeGebruikerToevoegen() {
 		Gebruiker gebruiker1 = new Gebruiker("jan@hu.nl", "sssadfsdfsdf", "jan");
 		school.addGebruiker(gebruiker1);
-		Gebruiker gebruiker2 = new Gebruiker("jan@hu.nl", "sssadfsdfsdf", "jan");
 		IllegalArgumentException thrown = assertThrows(
 				IllegalArgumentException.class,
-				() -> school.addGebruiker(gebruiker2),
+				() -> new Gebruiker("jan@hu.nl", "sssadfsdfsdf", "jan"),
 				"Zou een foutmelding moeten geven"
 		);
 
@@ -63,32 +60,31 @@ class SchoolTest {
 	void test_dezelfdeSubklasse_BestaandeGebruikerToevoegen() {
 		Student student1 = new Student("jan@hu.nl", "sssadfsdfsdf", "jan", 1234, slber);
 		school.addGebruiker(student1);
-		Student student2 = new Student("jan@hu.nl", "sssadfsdfsdf", "jan", 1234, slber);
 
 		IllegalArgumentException thrown = assertThrows(
 				IllegalArgumentException.class,
-				() ->             school.addGebruiker(student2),
-				"Gebruiker (" + student2 + ") bestaat al"
+				() -> new Student("jan@hu.nl", "sssadfsdfsdf", "jan", 1234, slber),
+				"fout bij controle op dubbele account bestaat al"
 		);
 
 		assertTrue(thrown.getMessage().contains("bestaat al"));
 	}
+
 	@Test
 	void test_slberNietInSchool() {
-		Docent slber2=new Docent("123@456.789","1233456434235","SlbTest",6432);
-		IllegalArgumentException thrown=assertThrows(IllegalArgumentException.class,()->new Student("jan@hu.nl", "sssadfsdfsdf", "jan", 1234, slber2),"Geen foutmelding bij selecteren van SLBer die niet in school zit");
-		assertEquals(thrown.getMessage(),"SLBer niet in School");
+		Docent slber2 = new Docent("123@456.789", "1233456434235", "SlbTest", 6432);
+		IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> new Student("jan@hu.nl", "sssadfsdfsdf", "jan", 1234, slber2), "Geen foutmelding bij selecteren van SLBer die niet in school zit");
+		assertEquals(thrown.getMessage(), "SLBer niet in School");
 	}
 
 	@Test
 	void test_verschillendeSubklasse_BestaandeGebruikerEmailToevoegen() {
 		Student student = new Student("jan@hu.nl", "sssadfsdfsdf", "jaasdfaisdfn", 123412, slber);
 		school.addGebruiker(student);
-		Docent docent = new Docent("jan@hu.nl", "345efgdgfdgf", "jan", 1234);
 
 		IllegalArgumentException thrown = assertThrows(
 				IllegalArgumentException.class,
-				() ->  school.addGebruiker(docent),
+				() -> new Docent("jan@hu.nl", "345efgdgfdgf", "jan", 1234),
 				"Gebruiker zou al gevonden moeten zijn."
 		);
 
@@ -101,7 +97,7 @@ class SchoolTest {
 		school.addGebruiker(student);
 		Docent docent = new Docent("jan2@hu.nl", "345efgdgfdgf", "jan", 1234);
 
-		assertDoesNotThrow(() ->  school.addGebruiker(docent), "ander email adres met zelfde naam zou geen probleem moeten zijn.");
+		assertDoesNotThrow(() -> school.addGebruiker(docent), "ander email adres met zelfde naam zou geen probleem moeten zijn.");
 	}
 
 	@Test
@@ -122,9 +118,9 @@ class SchoolTest {
 
 	@Test
 	public void test_BestaandeCursusToevoegen() {
-		Cursus cursus1 =  new Cursus("TCIF-V1GP-19_2019","SD-GroupProject");
+		Cursus cursus1 = new Cursus("TCIF-V1GP-19_2019", "SD-GroupProject");
 		school.addCursus(cursus1);
-		Cursus cursus2 =  new Cursus("TCIF-V1GP-19_2019","SD-GroupProject");
+		Cursus cursus2 = new Cursus("TCIF-V1GP-19_2019", "SD-GroupProject");
 
 		IllegalArgumentException thrown = assertThrows(
 				IllegalArgumentException.class,
@@ -139,8 +135,8 @@ class SchoolTest {
 
 	@Test
 	public void test_GebruikerGetByEmail() throws NotFoundException {
-		School school=School.getSchool();
-		Systeembeheerder systeembeheerder = new Systeembeheerder("jan@hu.nl", "sssadfsdfsdf","Jan de Man");
+		School school = School.getSchool();
+		Systeembeheerder systeembeheerder = new Systeembeheerder("jan@hu.nl", "sssadfsdfsdf", "Jan de Man");
 		school.addGebruiker(systeembeheerder);
 		assertEquals(school.getGebruikerByEmail("jan@hu.nl"), systeembeheerder);
 	}
@@ -159,7 +155,7 @@ class SchoolTest {
 
 	@Test
 	public void test_KlasGetByName() throws NotFoundException {
-		School school=School.getSchool();
+		School school = School.getSchool();
 		Klas klas = new Klas("TICT-SD-V1E");
 		school.addKlas(klas);
 		assertEquals(school.getKlasByName("TICT-SD-V1E"), klas);
@@ -168,7 +164,7 @@ class SchoolTest {
 
 	@Test
 	public void test_KlasGetByNameNotFound() {
-		School school=School.getSchool();
+		School school = School.getSchool();
 		Klas klas = new Klas("TICT-SD-V1E");
 		school.addKlas(klas);
 
@@ -183,8 +179,8 @@ class SchoolTest {
 
 	@Test
 	public void test_CursusGetByCode() throws NotFoundException {
-		School school=School.getSchool();
-		Cursus cursus =  new Cursus("TCIF-V1GP-19_2019","SD-GroupProject");
+		School school = School.getSchool();
+		Cursus cursus = new Cursus("TCIF-V1GP-19_2019", "SD-GroupProject");
 		school.addCursus(cursus);
 		assertEquals(school.getCursusByCode("TCIF-V1GP-19_2019"), cursus);
 	}
@@ -192,8 +188,8 @@ class SchoolTest {
 
 	@Test
 	public void test_CursusGetByCodeNotFound() {
-		School school=School.getSchool();
-		Cursus cursus =  new Cursus("TCIF-V1GP-19_2019","SD-GroupProject");
+		School school = School.getSchool();
+		Cursus cursus = new Cursus("TCIF-V1GP-19_2019", "SD-GroupProject");
 		school.addCursus(cursus);
 
 		NotFoundException thrown = assertThrows(
@@ -208,17 +204,17 @@ class SchoolTest {
 	@Test
 	public void test_serialize_deserialize() {
 		School.resetSchool();
-		assertDoesNotThrow(Main::initData,"Serialize zou geen foutmelding moeten geven");
-		School school1=School.getSchool();
+		assertDoesNotThrow(Main::initData, "Serialize zou geen foutmelding moeten geven");
+		School school1 = School.getSchool();
 		School.resetSchool();
 		//Gebruik van serializeDemoData omdat hier alle types al in staan, en dus ook getest worden.
-		assertDoesNotThrow(School::deserialize,"Deserialize zou geen foutmelding moeten geven");
-		assertEquals(School.getSchool(),school1,"School objecten zouden gelijk moeten zijn.");
+		assertDoesNotThrow(School::deserialize, "Deserialize zou geen foutmelding moeten geven");
+		assertEquals(School.getSchool(), school1, "School objecten zouden gelijk moeten zijn.");
 	}
 
 	@Test
 	public void test_removeBestaandeKlas() throws NotFoundException {
-		School school=School.getSchool();
+		School school = School.getSchool();
 		Klas klas = new Klas("TICT-SD-V1E");
 		school.addKlas(klas);
 		school.removeKlas(klas);
@@ -233,11 +229,11 @@ class SchoolTest {
 
 	@Test
 	public void test_removeNietBestaandeKlas() {
-		School school=School.getSchool();
+		School school = School.getSchool();
 		Klas klas = new Klas("TICT-SD-V1E");
 		NotFoundException thrown = assertThrows(
 				NotFoundException.class,
-				() ->  school.removeKlas(klas),
+				() -> school.removeKlas(klas),
 				"Klas niet gevonden"
 		);
 
@@ -248,8 +244,8 @@ class SchoolTest {
 
 	@Test
 	public void test_removeBestaandeCursus() throws NotFoundException {
-		School school=School.getSchool();
-		Cursus cursus =  new Cursus("TCIF-V1GP-19_2019","SD-GroupProject");
+		School school = School.getSchool();
+		Cursus cursus = new Cursus("TCIF-V1GP-19_2019", "SD-GroupProject");
 		school.addCursus(cursus);
 		school.removeCursus(cursus);
 
@@ -265,11 +261,11 @@ class SchoolTest {
 
 	@Test
 	public void test_removeNietBestaandeCursus() {
-		School school=School.getSchool();
-		Cursus cursus =  new Cursus("TCIF-V1GP-19_2019","SD-GroupProject");
+		School school = School.getSchool();
+		Cursus cursus = new Cursus("TCIF-V1GP-19_2019", "SD-GroupProject");
 		NotFoundException thrown = assertThrows(
 				NotFoundException.class,
-				() ->  school.removeCursus(cursus),
+				() -> school.removeCursus(cursus),
 				"Klas niet gevonden"
 		);
 
