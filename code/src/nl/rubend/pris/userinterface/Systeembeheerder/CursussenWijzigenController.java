3252b47460b855a5979e229be7093967141ca2f6
Ibrahim <ibrahim.aldarra@student.hu.nl>
2020-04-08 15:17:41 +0200

    curusus wijzigen is af

--- code/src/nl/rubend/pris/userinterface/Systeembeheerder/CursussenWijzigenController.java
+++ code/src/nl/rubend/pris/userinterface/Systeembeheerder/CursussenWijzigenController.java
@@ -1,13 +1,92 @@
 package nl.rubend.pris.userinterface.Systeembeheerder;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Button;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import nl.rubend.pris.model.*;
 import org.controlsfx.control.SearchableComboBox;
+
+import java.util.ArrayList;
 
 
 public class CursussenWijzigenController {
+
+    public SearchableComboBox<Cursus> CursusZoekComboBox;
+    public ListView listView;
+
+
+    private School school = School.getSchool();
+
+    private ArrayList<Docent> alleDocenten;
+    private ObservableList<String> dataList = FXCollections.observableArrayList();
+
+    public void initialize() throws Exception {
+        alleDocenten = new ArrayList<>();
+        for (Gebruiker gebruiker: school.getGebruikers()) {
+            if (gebruiker instanceof Docent) {
+                alleDocenten.add((Docent) gebruiker);
+            }
+        }
+        fillList();
+        for(Cursus cursus: school.getCursussen()) CursusZoekComboBox.getItems().add(cursus);
+
+    }
+
+    public void fillList() {
+        dataList.clear();
+        listView.getItems().clear();
+
+        for (Docent docent: alleDocenten) {
+            Cursus cursus = docent.getCursus();
+            if (cursus != null) {
+                dataList.add(docent.getNaam() + " : " + cursus.getCursusCode());
+            } else {
+                dataList.add(docent.getNaam());
+            }
+        }
+        listView.setItems(dataList);
+    }
+
+
+    public void handleMainPaneClicked(MouseEvent mouseEvent) {
+    }
+
+
+    public void handleVerwijderCursus(ActionEvent actionEvent) throws NotFoundException {
+        Object item = listView.getSelectionModel().getSelectedItem();
+        if (item != null) {
+            String[] parts = item.toString().split(" : ");
+            Cursus gesCursus = school.getCursusByCode(parts[1]);
+            for (Docent docent : alleDocenten) {
+                if (docent.getNaam().equals(parts[0])) {
+                    docent.setCursus(null);
+                    System.out.println(docent.getCursus());
+                    fillList();
+                }
+            }
+        }
+
+    }
+
+    public void handleZetCursus(ActionEvent actionEvent) throws NotFoundException {
+        Cursus gekCursus = CursusZoekComboBox.getValue();
+        Object item = listView.getSelectionModel().getSelectedItem();
+        if (gekCursus != null && item != null) {
+            if (!item.toString().contains(" : ")) {
+                for (Docent docent : alleDocenten) {
+                    if (docent.getNaam().equals(item)) {
+                        docent.setCursus(gekCursus);
+                        fillList();
+                    }
+                }
+            }
+        }
+    }
 
     public void handleSluit(ActionEvent actionEvent) {
         Button source = (Button)actionEvent.getSource();
@@ -15,15 +94,6 @@ public class CursussenWijzigenController {
         stage.close();
     }
 
-    public void handleMainPaneClicked(MouseEvent mouseEvent) {
-    }
 
-    public void handleCursusListClicked(MouseEvent mouseEvent) {
-    }
 
-    public void handleVerwijderCursus(ActionEvent actionEvent) {
-    }
-
-    public void handleZetCursus(ActionEvent actionEvent) {
-    }
 }
